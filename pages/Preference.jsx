52aeda0db34a6693abf02e751be0b235fcdf6ed8
namtran1812 <158846154+namtran1812@users.noreply.github.com>
2024-05-31 21:32:23 +0700

    slay

---
+++ pages/Preference.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "../components/nav/Nav";
+import Form from "../components/form/Form";
+
+function Preferences() {
+  return (
+    <>
+      <Nav />
+      <Form />
+    </>
+  );
+}
+
+export default Preferences;
