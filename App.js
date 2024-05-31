411f3d8f0f30fde3619c85e401926b3a79faf763
namtran1812 <158846154+namtran1812@users.noreply.github.com>
2024-05-31 21:35:59 +0700

    slay

---
+++ App.js
@@ -0,0 +1,22 @@
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import Preferences from "./pages/Preferences";
+import Result from "./pages/Result";
+import Signup from "./pages/Signup";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" Component={Homepage} />
+        <Route exact path="/login" Component={Login} />
+        <Route exact path="/Preferences" Component={Preferences} />
+        <Route exact path="/result" Component={Result} />
+        <Route exact path="/signup" Component={Signup} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
