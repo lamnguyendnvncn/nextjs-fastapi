0328625276649faa72822fd7d290f34d52897205
namtran1812 <158846154+namtran1812@users.noreply.github.com>
2024-05-31 21:30:37 +0700

    slay

---
+++ pages/Landingpage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "../components/nav/Nav";
+import Home from "../components/home/Home"
+
+function Landingpage() {
+  return (
+    <>
+      <Nav />
+      <Home />
+    </>
+  )
+}
+
+export default Landingpage;
